0a4cfedc6e61c2da84f2e62e186b5623efd18801
CoderPrans <pranavbhaskar17@gmail.com>
2019-02-25 23:39:51 +0530

    Updates

---
+++ precache-manifest.99f0103b8f9b98a26b129c43e6fa4ece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97c57429302b896b8c44",
+    "url": "/attendance/static/js/runtime~main.97c57429.js"
+  },
+  {
+    "revision": "7e636127a76c5a7db108",
+    "url": "/attendance/static/js/main.7e636127.chunk.js"
+  },
+  {
+    "revision": "015ea03cb7b11a05a1c5",
+    "url": "/attendance/static/js/2.015ea03c.chunk.js"
+  },
+  {
+    "revision": "7e636127a76c5a7db108",
+    "url": "/attendance/static/css/main.b1fc92c8.chunk.css"
+  },
+  {
+    "revision": "aede71dc6afd582ddcb6810a1aa300b1",
+    "url": "/attendance/index.html"
+  }
+];
